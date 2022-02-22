ba53ec26caf9cac7ebe89b8b49b7b57628c684a3
Connor Sutton <connorsutton07@gmail.com>
2022-02-21 21:55:14 -0600

    moved distribution to make it more accessible

---
+++ core/distribution.py
@@ -0,0 +1,11 @@
+import random 
+
+class Distribution:
+    def __init__(self, choices: list, smoothing: float = 2) -> None:
+        # the higher the base, the more the smoothing
+        assert smoothing > 1
+        self.counts = {choice: log(choices.count(choice) + 1, smoothing) for choice in set(choices)}
+        self.size = len(self.counts)
+
+    def sample(self):
+        return random.choices(list(self.counts.keys()), weights = list(self.counts.values()), k = 1)[0]
